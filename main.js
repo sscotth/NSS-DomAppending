2c516cefa307adeb8148377abdfd00254d31c1c8
Scott <sscotth@gmail.com>
2014-10-16 11:46:11 -0500

    use docFragments

--- main.js
+++ main.js
@@ -2,16 +2,22 @@ document.addEventListener('DOMContentLoaded', function(){
   var $button = document.querySelector('button');
 
   $button.addEventListener('click', function(){
-      var $target = document.querySelector('.target');
-
-      var $div = document.createElement('div');
-      $div.innerHTML = '<p>Hi</p>';
-
-      $target.appendChild($div);
-      
+    var $target = document.querySelector('.target');
+    var docFragment = createPTag();
+    $target.appendChild(docFragment);
   });
 
-
-
-
 });
+
+function createPTag(){
+  var docFragment = document.createDocumentFragment();
+
+  var $div = document.createElement('div');
+  $div.setAttribute('class', 'myClass');
+  var $p = document.createElement('p');
+  $div.appendChild($p);
+  $p.textContent = 'Hi';
+  docFragment.appendChild($div);
+
+  return docFragment;
+}
